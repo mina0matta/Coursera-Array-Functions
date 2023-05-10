dffae2a76be6e60cc4c4185003fb00b1f7aaaccc
Mina Ihab Matta <mina.matta3000hero@gmail.com>
2023-05-10 16:01:23 +0300

    Adding Code Outline and Documentation for C1M1

--- stats.c
+++ stats.c
@@ -1,25 +1,25 @@
 /******************************************************************************
- * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ * Copyright (C) 2023 by Mina Ihab Matta
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
- * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * software. Mina Ihab is not liable for any
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * stats.c
+ * Implementation file for the C-programming code
  *
- * <Add Extended Description Here>
+ * A program that can analyze an array of unsigned char data items and report analytics on the maximum,minimum, mean, and median of the data set.
+ * In addition reorder this data set from large to small.
+ * All statistics are rounded down to the nearest integer.
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * Mina Ihab Matta
+ * 10-05-2023
  *
  */
-
-
 
 #include <stdio.h>
 #include "stats.h"
@@ -27,17 +27,39 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
-
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
-
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
-
+void main()
+{
+  unsigned char test[SIZE] = {34, 201, 190, 154, 8, 194, 2, 6,
+                              114, 88, 45, 76, 123, 87, 25, 23,
+                              200, 122, 150, 90, 92, 87, 177, 244,
+                              201, 6, 12, 60, 8, 2, 5, 67,
+                              7, 87, 250, 230, 99, 3, 100, 90};
 }
 
-/* Add other Implementation File Code Here */
+void print_statistics(unsigned char *arr, unsigned int length)
+{
+}
+
+void print_array(unsigned char *arr, unsigned int length)
+{
+}
+
+unsigned char find_median(unsigned char *arr, unsigned int length)
+{
+}
+
+unsigned char find_mean(unsigned char *arr, unsigned int length)
+{
+}
+
+unsigned char find_maximum(unsigned char *arr, unsigned int length)
+{
+}
+
+unsigned char find_minimum(unsigned char *arr, unsigned int length)
+{
+}
+
+void sort_array(unsigned char *arr, unsigned int length)
+{
+}
--- stats.h
+++ stats.h
@@ -1,40 +1,94 @@
 /******************************************************************************
- * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ * Copyright (C) 2023 by Mina Ihab Matta
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
- * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * software. Mina Ihab is not liable for any
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * stats.h
+ * Header File for the C-programming code
  *
- * <Add Extended Description Here>
+ * A file containing all the declarations of the functions used in the main Code file
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * Mina Ihab Matta
+ * 10-05-2023
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
-
+void print_statistics(unsigned char *arr, unsigned int length);
 /**
- * @brief <Add Brief Description of Function Here>
+ * A function that prints the statistics of an array including minimum, maximum, mean, and median.
  *
- * <Add Extended Description Here>
+ * unsigned char* arr   unsigned char pointer to an n-element data array
+ * unsigned int length  unsigned integer as the size of the array
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * returns void
  */
 
+void print_array(unsigned char *arr, unsigned int length);
+/**
+ * A function that prints the array to the screen
+ *
+ * unsigned char* arr   unsigned char pointer to an n-element data array
+ * unsigned int length  unsigned integer as the size of the array
+ *
+ * returns void
+ * */
+
+unsigned char find_median(unsigned char *arr, unsigned int length);
+/**
+ * A function that returns the median value
+ *
+ * unsigned char* arr   unsigned char pointer to an n-element data array
+ * unsigned int length  unsigned integer as the size of the array
+ *
+ * returns median value as unsigned char
+ */
+
+unsigned char find_mean(unsigned char *arr, unsigned int length);
+/**
+ * A function that returns the mean value
+ *
+ * unsigned char* arr   unsigned char pointer to an n-element data array
+ * unsigned int length  unsigned integer as the size of the array
+ *
+ * returns mean value as unsigned char
+ */
+
+unsigned char find_maximum(unsigned char *arr, unsigned int length);
+/**
+ * A function that returns the maximum value
+ *
+ * unsigned char* arr   unsigned char pointer to an n-element data array
+ * unsigned int length  unsigned integer as the size of the array
+ *
+ * returns maximum value as unsigned char
+ */
+
+unsigned char find_minimum(unsigned char *arr, unsigned int length);
+/**
+ * A function that returns the minimum value
+ *
+ * unsigned char* arr   unsigned char pointer to an n-element data array
+ * unsigned int length  unsigned integer as the size of the array
+ *
+ * returns minimum value as unsigned char
+ */
+
+void sort_array(unsigned char *arr, unsigned int length);
+/**
+ * A function that sorts the array from largest value to smallest.
+ *
+ * unsigned char* arr   unsigned char pointer to an n-element data array
+ * unsigned int length  unsigned integer as the size of the array
+ *
+ * returns void
+ */
 
 #endif /* __STATS_H__ */
